5f92e2cbefa23a254714da6a019113b07dc1e234
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2015-10-29 19:43:01 +0100

    Print usage string to stderr.

--- wordvalue.c
+++ wordvalue.c
@@ -23,7 +23,7 @@
 int main(int argc, char **argv)
 {
 	if (argc > 2) {
-		printf("Usage: %s [input_file]\n", argv[0]);
+		fprintf(stderr, "Usage: %s [input_file]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
